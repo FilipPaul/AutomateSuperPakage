7d510c8fc7f18b33648b8ecf762fa95e0529aae1
Filip Paul <filip.paul@email.cz>
2021-09-10 19:57:57 +0200

    sync

--- AutomateSuperPackage/DatabasePackage/AccesDatabasePackage/AccesDatabaseModule.py
+++ AutomateSuperPackage/DatabasePackage/AccesDatabasePackage/AccesDatabaseModule.py
@@ -4,12 +4,49 @@ import pyodbc
 class AccesDatabaseClass:
     def __init__(self):
         print("IM AccesDatabaseClass")
+        self.MultipleQueryResult = []
+        self.connections = []
+        self.cursors = []
+
+    def multipleCursors(self,list_of_databases):
+        for members in list_of_databases:
+            con_str = "Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=" +members
+            self.connections.append(pyodbc.connect(con_str))
+            self.cursors.append(self.connections[-1].cursor())
+        self.cursor = self.cursors[0]
+
+    def MultipleWriteQuery(self, QueryString,cursor_positions):
+        self.Query = QueryString
+        if len(cursor_positions) == 1:
+            self.cursors[cursor_positions[0]].execute(self.Query)
+        else:
+            for position in cursor_positions:
+                print(position)
+                self.cursors[position].execute(self.Query)
+
+    def MultipleResultFromQuery(self,cursor_positions):
+        self.MultipleQueryResult.clear()
+        if len(cursor_positions) == 1:
+            return self.cursors[cursor_positions[0]].fetchall()
+
+        for position in cursor_positions:
+            self.MultipleQueryResult.append(self.cursors[position].fetchall())
+        return self.MultipleQueryResult
+    
+    def MultipleUpdateDatabase(self,cursor_positions):
+        if len(cursor_positions) == 1:
+            self.cursors[cursor_positions[0]].commit()
+        else:
+            for position in cursor_positions:
+                self.cursors[position].commit()
+
 
     def SimplyConnectByPath(self,path):
         self.dBpath = os.path.abspath(path)
         self.ConnectionString = "Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=" + self.dBpath + ";"
         self.connection = pyodbc.connect(self.ConnectionString)
         self.cursor = self.connection.cursor()
+        
 
     def WriteQuery(self, QueryString):
         self.Query = QueryString
